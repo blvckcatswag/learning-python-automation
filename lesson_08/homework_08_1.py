852ef57b9de572b54b7eac34a430c7ad46b1aac1
Aleksei Masenko <bestqaengineer7@gmail.com>
2025-07-15 23:49:07 +0300

    Домашнее задание 8 к уроку 8

--- lesson_08/homework_08_1.py
+++ lesson_08/homework_08_1.py
@@ -1,4 +1,8 @@
 def sum_numbers_from_string(s: str) -> str:
+    """
+    Преобразует строку с числами, разделёнными запятой, в сумму этих чисел.
+    Если строка содержит некорректные данные — возвращает сообщение об ошибке.
+    """
     try:
         numbers = [float(item) for item in s.split(',')]
         total = int(sum(numbers))
@@ -6,8 +10,8 @@ def sum_numbers_from_string(s: str) -> str:
     except ValueError:
         return "Не можу це зробити"
 
-# Тестовый список
-data = ["1,2,3,4", "12,3,4.50", "qwerty,1,2,3"]
+
+data = ["1,2,3,4", "1,2,3,4,50", "qwerty1,2,3"]
 
 for item in data:
     print(sum_numbers_from_string(item))
